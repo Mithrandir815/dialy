c66acd9344db8e743a4ca2a243b260e0e4b7bdfd
Takayuki Iida <allthamoon@yahoo.co.jp>
2019-06-23 04:16:12 +0900

    final

--- nikki/urls.py
+++ nikki/urls.py
@@ -5,7 +5,7 @@ app_name = 'nikki'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('add/', views.add, name='add'),
-    path('upadate/<int:pk>/', views.update, name='update'),
-    path('delete/<int:pk>', views.delete, name='delete'),
+    path('add/', views.AddView.as_view(), name='add'),
+    path('upadate/<int:pk>/', views.UpdateView.as_view(), name='update'),
+    path('delete/<int:pk>', views.DeleteView.as_view(), name='delete'),
 ]
--- nikki/views.py
+++ nikki/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
 from django.views import generic
 from .forms import DayCreateForm
 from .models import Day
@@ -6,51 +7,20 @@ from .models import Day
 
 class IndexView(generic.ListView):
     model = Day
+    paginate_by = 3
+    #template_name = 'nikki/my_list.html'もし違う名前でテンプレートを作成たい場合
 
-def add(request):
-    #送信内容を基にフォームを作成する、POSTじゃなければ。からのフォーム
-    form = DayCreateForm(request.POST or None)
+class AddView(generic.CreateView):
+    model = Day
+    form_class = DayCreateForm
+    template_name = 'nikki/day_forms.html'
+    success_url = reverse_lazy('nikki:index')
 
-    #method=POST,つまり送信ボタン押下時、入力内容が問題なければ
-    if request.method == 'POST' and form.is_valid():
-        form.save()
-        return redirect('nikki:index')
+class UpdateView(generic.UpdateView):
+    model = Day
+    form_class = DayCreateForm
+    success_url = reverse_lazy('nikki:index')
 
-    #通常時のページアクセスや、入力内容に誤りがあればまたページを表示する
-    context = {
-        'form':form
-    }
-    return render(request, 'nikki/day_forms.html', context)
-
-def update(request, pk):
-    #get Day base from url' pk
-    day = get_object_or_404(Day, pk=pk)
-
-    #getting Day bond to forms
-    form = DayCreateForm(request.POST or None, instance=day)
-
-    #method=POST so it do when push the send button if contents inputted haven't problem
-    if request.method == 'POST' and form.is_vaild():
-        form.save()
-        return redirect('nikki:index')
-
-    #When nomal pageaccece or contents inputed dosen't have mistakes
-    context = {
-        'form':form
-    }
-    return render(request, 'nikki/day_forms.html', context)
-
-def delete(request, pk):
-    #get Day base from url' pk
-    day = get_object_or_404(Day, pk=pk)
-
-    #method=POST so it do when push the send button if contents inputted haven't problem
-    if request.method == 'POST':
-        day.delete()
-        return redirect('nikki:index')
-
-    #When nomal pageaccece or contents inputed dosen't have mistakes
-    context = {
-        'day':day
-    }
-    return render(request, 'nikki/day_confirm_delete.html', context)
+class DeleteView(generic.DeleteView):
+    model = Day
+    success_url = reverse_lazy('nikki:index')
